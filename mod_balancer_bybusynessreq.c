f369e6a33ac146bef4bbc6e04629e29fa1c30cc7
Toshiyuki Suzumura <suz.labo@amail.plala.or.jp>
2011-04-06 14:01:26 +0900

    Add timeout config.

--- mod_balancer_bybusynessreq.c
+++ mod_balancer_bybusynessreq.c
@@ -16,6 +16,7 @@
  */
 
 #include "httpd.h"
+#include "http_config.h"
 #include "mod_proxy.h"
 #include "ap_mpm.h"
 #include <curl/curl.h>
@@ -28,21 +29,37 @@
 #define AP_LOG_ERR(rec, fmt, ...)   ap_log_error(APLOG_MARK, APLOG_ERR,    0, rec, "[" LBMETHOD "] " fmt, ##__VA_ARGS__)
 
 
+module AP_MODULE_DECLARE_DATA balancer_bybusynessreq_module;
+
+// Config store
+typedef struct _lbmethod_conf {
+  int timeout;    // HEAD request timeout by sec.
+} lbmethod_conf;
+static const lbmethod_conf DEFAULT_CONFIG = { 5 };
+
+
 /*
  * heartbeat_handler for backend-apache.
  */
-static int request_to_backend(proxy_worker *worker, server_rec *rec)
+static int request_to_backend(proxy_worker* worker, server_rec* rec)
 {
   int threaded_mpm;
   int code;
   CURLcode ret;
   CURL* curl = curl_easy_init();
+  lbmethod_conf* conf = (lbmethod_conf*)ap_get_module_config(rec->module_config, &balancer_bybusynessreq_module);
+  if(conf==NULL) {
+    AP_LOG_WARN(rec, "config object not found.");
+    conf = &DEFAULT_CONFIG;
+  }
 
   ap_mpm_query(AP_MPMQ_IS_THREADED, &threaded_mpm);
   curl_easy_setopt(curl, CURLOPT_NOSIGNAL, threaded_mpm);
   curl_easy_setopt(curl, CURLOPT_URL, worker->name);
   curl_easy_setopt(curl, CURLOPT_CUSTOMREQUEST, "HEAD");
   curl_easy_setopt(curl, CURLOPT_FOLLOWLOCATION, 0);
+  curl_easy_setopt(curl, CURLOPT_TIMEOUT, conf->timeout);
+  curl_easy_setopt(curl, CURLOPT_CONNECTTIMEOUT, conf->timeout);
   ret = curl_easy_perform(curl);
   curl_easy_getinfo(curl, CURLINFO_RESPONSE_CODE, &code);
   curl_easy_cleanup(curl);
@@ -55,7 +72,7 @@ static int request_to_backend(proxy_worker *worker, server_rec *rec)
 }
 
 
-static int proxy_check_worker(const char *proxy_function, proxy_worker *worker, server_rec *rec)
+static int proxy_check_worker(const char *proxy_function, proxy_worker* worker, server_rec* rec)
 {
   if(worker->s->status & PROXY_WORKER_IN_ERROR) {
     AP_LOG_DEBUG(rec, "proxy: %s: retrying the worker for (%s)", proxy_function, worker->hostname);
@@ -79,7 +96,7 @@ static int proxy_check_worker(const char *proxy_function, proxy_worker *worker, 
 /*
  * ProxySet lbmethod=bybusynessreq
  */
-static proxy_worker *find_best_bybusyness_req(proxy_balancer *balancer, request_rec *rec)
+static proxy_worker *find_best_bybusyness_req(proxy_balancer* balancer, request_rec* rec)
 {
   int i;
   proxy_worker *mycandidate = NULL;
@@ -108,7 +125,7 @@ static proxy_worker *find_best_bybusyness_req(proxy_balancer *balancer, request_
           worker->s->lbstatus += worker->s->lbfactor;
           total_factor += worker->s->lbfactor;
                     
-          if (!mycandidate
+          if(!mycandidate
               || worker->s->busy < mycandidate->s->busy
               || (worker->s->busy == mycandidate->s->busy && worker->s->lbstatus > mycandidate->s->lbstatus))
               mycandidate = worker;
@@ -136,17 +153,31 @@ static const proxy_balancer_method bybusynessreq =
     NULL
 };
 
-static void balancer_bybusynessreq_register_hook(apr_pool_t *pool)
+static void balancer_bybusynessreq_register_hook(apr_pool_t* pool)
 {
   ap_register_provider(pool, PROXY_LBMETHOD, LBMETHOD, "0", &bybusynessreq);
 }
 
+
+static void* config_create(apr_pool_t* pool, char* d)
+{
+  lbmethod_conf* conf = (lbmethod_conf*)apr_palloc(pool, sizeof(lbmethod_conf));
+  memcpy(conf, &DEFAULT_CONFIG, sizeof(*conf));
+  return conf;
+}
+
+static const command_rec config_cmds[] = {
+  AP_INIT_TAKE1(LBMETHOD "-timeout", ap_set_int_slot, \
+    (void*)APR_OFFSETOF(lbmethod_conf, timeout), OR_OPTIONS, "HEAD request timeout."),
+  { NULL }
+};
+
 module AP_MODULE_DECLARE_DATA balancer_bybusynessreq_module = {
     STANDARD20_MODULE_STUFF,
-    NULL,       /* create per-directory config structure */
-    NULL,       /* merge per-directory config structures */
-    NULL,       /* create per-server config structure */
-    NULL,       /* merge per-server config structures */
-    NULL,       /* command apr_table_t */
+    config_create,  /* create per-directory config structure */
+    NULL,           /* merge per-directory config structures */
+    NULL,           /* create per-server config structure */
+    NULL,           /* merge per-server config structures */
+    config_cmds,    /* command apr_table_t */
     balancer_bybusynessreq_register_hook /* register hooks */
 };
